97e36f0a0e8cc218c6b2f8689036eeab09b6ec27
MariusPricope <mariuspricope66@gmai.com>
2022-05-02 20:22:40 +0300

    verify if the username respect all rules. verify if email contain uppercase letters

--- Script/sign-in.js
+++ Script/sign-in.js
@@ -7,11 +7,52 @@ var e_error = document.getElementById("error_email");
 var password = document.getElementById("password");
 var pw_error = document.getElementById("error_password");
 
+///list of errors
+
+
 ///If sign-in button is clicked will be called verify function
 signin_button.onclick = function () {
-    Verify(username,"username",un_error);
-    Verify(email,"email", e_error);
-    Verify(password,"password",pw_error);
+    //submit boolen variable
+    var submit = true;
+    //hide errors
+    HideErrors(un_error);
+    HideErrors(e_error);
+    HideErrors(pw_error);
+
+    //verify if username respect rules
+    if(Empty(username,"username",un_error) === false)
+    {
+        if(CorrectUsername() === false)
+        {
+            submit = false;
+        }
+    }
+    else{
+        submit = false;
+    }
+
+    if(Empty(email,"email", e_error) === false)
+    {
+        if(CorrectEmail() === false)
+        {
+            submit = false;
+        }
+    }
+    else {
+        submit = false;
+    }
+
+    if(Empty(password,"password",pw_error) === false)
+    {
+
+    }
+    else {
+        submit = false;
+    }
+
+    if(submit === true) {
+        signin_button.type = "submit";
+    }
 }
 
 ///Verify if first letter is a vowel
@@ -21,7 +62,7 @@ function Vowel(letter)
 } 
 
 /// Verify if input field is empthy or not
-function Verify(object, name, error)
+function Empty(object, name, error)
 {
     if(object.value.length == 0)
     {
@@ -36,12 +77,53 @@ function Verify(object, name, error)
         
         //change display mode to show error message
         error.style.display = "flex";
+        return true;
 
     }
     else{
-        //if all fields isn't empty, change button type for submit information
-        signin_button.type = "submit";
-        console.log(signin_button.type);
-        error.style.display = "none";
+        return false;
     }
+}
+
+//this function verify if username is matched with rules
+function CorrectUsername()
+{
+    if(username.value.length < 5 || username.value.length > 15)
+    {
+        un_error.textContent = "Username should have between 5 and 15 characters";
+        un_error.style.display = "flex";
+        return false;
+    }
+
+    for(let i = 0; i < username.value.length; i++)
+    {
+        if(username.value[i] === ' ')
+        {
+            un_error.textContent = "Username shouldn't contain spaces";
+            un_error.style.display = "flex";
+            return false;
+        }
+    }
+}
+
+//verify if an email is correct
+function CorrectEmail() {
+    
+    for(let i = 0 ; i < email.value.length; i++)
+    {
+        if(email.value[i] >= 'A' && email.value[i] <= 'Z')
+        {
+            e_error.textContent = "Email shouldn't contain uppercase letters";
+            e_error.style.display = "flex";
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// hide all errors
+function HideErrors(error)
+{
+    error.style.display = "none";
 }
